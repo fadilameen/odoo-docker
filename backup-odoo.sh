41c46fef0bf0a260380d48aba7f84927e1bd82b6
fadilameen <fadilameen63@gmail.com>
2025-02-19 13:47:32 +0530

    some issue with filestore backup creation file not found error

--- backup-odoo.sh
+++ backup-odoo.sh
@@ -8,9 +8,11 @@ DB_CONTAINER_ID="odoo-docker-setup_db-main_1"
 
 HOST_PATH="/home/cybrosys/odoo-docker-setup/backup"
 
-CONTAINER_PATH=""
+CONTAINER_PATH="/tmp"
 
-FILESTORE_BACKUP_NAME="odoo_filestore_backup_$(date +%Y-%m-%d_%H-%M-%S)"
+FILESTORE_BACKUP_NAME="odoo_filestore_backup_$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
+# FILESTORE_BACKUP_NAME="odoo_filestore_backup"
+
 
 DB_BACKUP_NAME="odoo_db_backup_`date +%Y-%m-%d"_"%H_%M_%S`.sql"
 
@@ -18,15 +20,15 @@ DB_BACKUP_NAME="odoo_db_backup_`date +%Y-%m-%d"_"%H_%M_%S`.sql"
 # Database Backup
 
 docker exec -t $DB_CONTAINER_ID pg_dumpall -c -U odoo18-main > $HOST_PATH/$DB_BACKUP_NAME
-echo "db backup complete"
 
 # Filestore Backup
 
-sudo docker exec -it --user root $CONTAINER_ID tar -cvzf $CONTAINER_PATH/$FILESTORE_BACKUP_NAME.tar.gz -C /var/lib odoo
+
+docker exec -it $CONTAINER_ID tar -cvzf $CONTAINER_PATH/$FILESTORE_BACKUP_NAME -C /var/lib odoo
 # sudo docker exec -it --user root $CONTAINER_ID tar -cvzf /odoo_backup_test_2.tar.gz -C /var/lib odoo
 
 
-sudo docker cp $CONTAINER_ID:$CONTAINER_PATH/$FILESTORE_BACKUP_NAME.tar.gz $HOST_PATH
+docker cp $CONTAINER_ID:$CONTAINER_PATH/$FILESTORE_BACKUP_NAME $HOST_PATH
 # sudo docker cp $CONTAINER_ID:odoo_backup_test_2.tar.gz $HOST_PATH
 
 # Optional: Remove old backups, keeping last 3
